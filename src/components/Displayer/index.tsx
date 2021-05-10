239646e08a399b25d2356402c74f5ab8d65e132b
Jay <jmartinsmith@outlook.com>
2021-05-10 16:03:18 +0000

    end of day commit

--- src/components/Displayer/index.tsx
+++ src/components/Displayer/index.tsx
@@ -1,20 +1,30 @@
 
 import Instructor from '../../domain/instructor';
+import { useState } from 'react';
+import { Combination } from '../../domain/combination';
 
 interface ComboDetails {
     details: Instructor
 };
 
-const OFFENSIVE_YIELD = 7;
+//percentage at which the next combo will be an attack
+const OFFENSIVE_YIELD = 70;
 
-const onOffense = (): boolean => (Math.floor(Math.random() * OFFENSIVE_YIELD) + 1 <= OFFENSIVE_YIELD);
+const onOffense = (): boolean => (Math.floor(Math.random() * 100) + 1) <= OFFENSIVE_YIELD;
 
 const DisplayField: React.FunctionComponent<ComboDetails> = (props) => {
     const { details } = props;
+    const [delta, setDelta] = useState(0);
+    const [onOffensive, setOnOffensive] = useState(onOffense());
+    
+    const getCombo = ():Combination  => {
+        return details.offensiveCombinations[delta];
+    };
+
     return (
         <div>
-            <p> on offense? {onOffense || "yes"}</p>
-            {JSON.stringify(details.offensiveCombinations)}
+            <p>{onOffense() ? ("Attack"!) : ("Defend!")}</p>
+            {JSON.stringify(getCombo())}
         </div>
             
     )
